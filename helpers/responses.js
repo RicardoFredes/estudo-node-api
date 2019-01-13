7ca820778f55eb825ebd92c442ed981e16d17025
ricardo.fredes@mesalva.com <ricardofredes@gmail.com>
2019-01-13 16:13:16 -0200

    Organize responses

---
+++ helpers/responses.js
@@ -0,0 +1,25 @@
+const getMetaPagination = (req, count, limit) => {
+  const page = Number(req.query.page)
+  const path = req.route.path
+  const pages = Math.ceil(count/limit)
+  const links = { current: req.url }
+
+  if (page) {
+    const lastPage = page - 1
+    links.last = `${path}?page=${lastPage}`
+  }
+  if (page < pages) {
+    const nextPage = page + 1
+    links.next = `${path}?page=${nextPage}`
+  }
+  return { links, pages }
+}
+
+const sendResponse = (res, data, parser, meta) => {
+  if (data.length) meta.count = data.length
+  const normalizeData = !data.length ? [data] : data
+  const parseData = normalizeData.map(parser)
+  return res.status(201).json({ meta, data: parseData })
+}
+
+module.exports = { sendResponse, getMetaPagination }
